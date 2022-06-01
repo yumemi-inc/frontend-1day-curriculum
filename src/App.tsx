e184810285823f57be67361be052c2135ffe115f
Umisyo <s_kusunoki@yumemi.co.jp>
2022-06-01 15:13:13 +0900

    wip: makeNewStateを動くようにしたい

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,32 @@
-import './App.css';
-import { useState, useEffect } from 'react';
-import getPrefData from './components/prefAPI';
-import getPopData from './components/popAPI';
-import Highcharts from 'highcharts';
-import HighchartsReact from 'highcharts-react-official';
-import NoDataToDisplay from 'highcharts/modules/no-data-to-display';
+import './App.css'
+import { useState, useEffect } from 'react'
+import getPrefData from './components/prefAPI'
+import Highcharts from 'highcharts'
+import HighchartsReact from 'highcharts-react-official'
+import NoDataToDisplay from 'highcharts/modules/no-data-to-display'
+import { makeNewStates } from './makeNewStates'
 
-NoDataToDisplay(Highcharts);
+NoDataToDisplay(Highcharts)
 
 type PrefData = {
-  prefCode: number;
-  prefName: string;
-};
+  prefCode: number
+  prefName: string
+}
 
 const App: React.FC = () => {
-  const [prefectures, setPrefectures] = useState<PrefData[]>([]);
-  const [checkedPrefCodes, setCheckedPrefCodes] = useState<number[]>(
-    []
-  );
+  const [prefectures, setPrefectures] = useState<PrefData[]>([])
+  const [checkedPrefCodes, setCheckedPrefCodes] = useState<number[]>([])
   const [loadedPrefData, setLoadedPrefData] = useState(
     new Map<number, number[]>()
-  );
+  )
 
-  const graphData: { data: number[]; name: string }[] =
-    checkedPrefCodes
-      .map((code) =>
-        prefectures.find((pref) => pref.prefCode === code)
-      )
-      .filter(
-        (pref) =>
-          pref !== undefined && loadedPrefData.has(pref.prefCode)
-      )
-      .map((pref) => ({
-        name: pref!.prefName,
-        data: [...loadedPrefData.get(pref!.prefCode)!],
-      }));
+  const graphData: { data: number[]; name: string }[] = checkedPrefCodes
+    .map((code) => prefectures.find((pref) => pref.prefCode === code))
+    .filter((pref) => pref !== undefined && loadedPrefData.has(pref.prefCode))
+    .map((pref) => ({
+      name: pref!.prefName,
+      data: [...loadedPrefData.get(pref!.prefCode)!],
+    }))
 
   const options = {
     chart: {
@@ -107,67 +99,47 @@ const App: React.FC = () => {
       },
     },
     series: graphData,
-  };
+  }
 
   useEffect(() => {
-    getPrefData.GetPref().then((data) => setPrefectures(data));
-  }, []);
+    getPrefData.GetPref().then((data) => setPrefectures(data))
+  }, [])
 
-  const handleChange = (checked: boolean, prefCode: number) => {
-    if (checked) {
-      if (!checkedPrefCodes.includes(prefCode)) {
-        setCheckedPrefCodes([...checkedPrefCodes, prefCode]);
-      }
-      if (!loadedPrefData.has(prefCode)) {
-        getPopData.FetchPop(prefCode).then((res) => {
-          setLoadedPrefData((oldData) => {
-            const newData = new Map(oldData);
-            newData.set(
-              prefCode,
-              res[0].data.map((item: any) => item.value)
-            );
-            return newData;
-          });
-        });
-      }
-    } else {
-      setCheckedPrefCodes(
-        checkedPrefCodes.filter((code) => code !== prefCode)
-      );
-    }
-  };
+  const handleChange = async (checked: boolean, prefCode: number) => {
+    setCheckedPrefCodes(async (oldData) => {
+      return (await makeNewStates(checked, prefCode, oldData, loadedPrefData))
+        .fetchedNewLoadData
+    })
+    setLoadedPrefData(async (oldData) => {
+      return (await makeNewStates(checked, prefCode, checkedPrefCodes, oldData))
+        .fetchedNewLoadData
+    })
+  }
 
   return (
-    <div className="container">
-      <h1 className="container-title">
+    <div className='container'>
+      <h1 className='container-title'>
         <span>都道府県別の総人口推移グラフ</span>
       </h1>
-      <h3 className="container-main">
+      <h3 className='container-main'>
         <span>都道府県</span>
       </h3>
 
-      <div className="app-prefectures-list-container">
+      <div className='app-prefectures-list-container'>
         {prefectures?.map((item) => {
           return (
-            <label
-              key={item.prefCode}
-              className="app-prefectures-list"
-            >
+            <label key={item.prefCode} className='app-prefectures-list'>
               <input
-                className="app-prefectures-list-checkbox"
-                type="checkbox"
-                onChange={(e) =>
-                  handleChange(e.target.checked, item.prefCode)
-                }
+                className='app-prefectures-list-checkbox'
+                type='checkbox'
+                onChange={(e) => handleChange(e.target.checked, item.prefCode)}
               />
-              <span className="app-prefectures-name">
-                {item.prefName}
-              </span>
+              <span className='app-prefectures-name'>{item.prefName}</span>
             </label>
-          );
+          )
         })}
       </div>
-      <div className="container-chart">
+      <div className='container-chart'>
         <HighchartsReact
           highcharts={Highcharts}
           constructorType={'chart'}
@@ -175,7 +147,7 @@ const App: React.FC = () => {
         />
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
---
+++ src/makeNewStates.ts
@@ -0,0 +1,34 @@
+import getPopData from './components/popAPI'
+
+export const makeNewStates = async (
+  checked: boolean,
+  prefCode: number,
+  checkedPrefCodes: number[],
+  loadedPrefData: Map<number, number[]>
+) => {
+  const newStates = {
+    newCheckedPrefCodes: checkedPrefCodes,
+    fetchedNewLoadData: loadedPrefData,
+  }
+  if (checked) {
+    if (!checkedPrefCodes.includes(prefCode)) {
+      newStates.newCheckedPrefCodes = [...checkedPrefCodes, prefCode]
+    }
+    if (!loadedPrefData.has(prefCode)) {
+      const res = await getPopData.FetchPop(prefCode)
+
+      let newLoadedData = new Map(loadedPrefData)
+      newLoadedData.set(
+        prefCode,
+        res[0].data.map((item: any) => item.value)
+      )
+
+      newStates.fetchedNewLoadData = newLoadedData
+    }
+    return newStates
+  } else {
+    newStates.newCheckedPrefCodes = checkedPrefCodes.filter(
+      (code) => code !== prefCode
+    )
+  }
+}
