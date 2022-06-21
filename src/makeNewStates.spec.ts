a214f36f239d75ef1113934d5fc29d826f69901c
Umisyo <s_kusunoki@yumemi.co.jp>
2022-06-21 14:29:03 +0900

    feat: testを追加

---
+++ src/makeNewStates.spec.ts
@@ -0,0 +1,13 @@
+import { makeNewStates } from './makeNewStates'
+
+describe('makeNewStates', () => {
+  test('not checked', async () => {
+    const response = await makeNewStates(false, 47, [], new Map())
+    expect(response).toEqual({
+      fetchedNewLoadData: new Map(),
+      newCheckedPrefCodes: [],
+    })
+  })
+})
+
+export {}
