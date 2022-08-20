9d1dca483011e50e270eeb159ca8154bda01c5b1
haru <haruki6180@gmail.com>
2022-08-20 16:24:35 +0900

    makeNewStatesの修正

--- src/makeNewStates.ts
+++ src/makeNewStates.ts
@@ -2,34 +2,42 @@ import {FetchPopulation} from './components/fetchPopulationAPI';
 
 export const makeNewStates = async (
   checked: boolean,
-  prefCode: number,
+  newCheckedPrefCode: number,
   checkedPrefCodes: number[],
-  loadedPrefData: Map<number, number[]>,
+  loadedPrefDataCache: Map<number, number[]>,
 ) => {
   const newStates = {
     newCheckedPrefCodes: checkedPrefCodes,
-    fetchedNewLoadData: loadedPrefData,
+    fetchedNewLoadDataCache: loadedPrefDataCache,
   };
-  if (checked) {
-    if (!checkedPrefCodes.includes(prefCode)) {
-      newStates.newCheckedPrefCodes = [...checkedPrefCodes, prefCode];
-    }
-    if (!loadedPrefData.has(prefCode)) {
-      const res = await FetchPopulation(prefCode);
 
-      const newLoadedData = new Map(loadedPrefData);
-      newLoadedData.set(
-        prefCode,
-        res[0].data.map((item: any) => item.value),
-      );
-
-      newStates.fetchedNewLoadData = newLoadedData;
-    }
-    return newStates;
-  } else {
-    newStates.newCheckedPrefCodes = checkedPrefCodes.filter(
-      (code) => code !== prefCode,
-    );
-    return newStates;
+  if (checked && !loadedPrefDataCache.has(newCheckedPrefCode)) {
+    newStates.fetchedNewLoadDataCache = await updateLoadedDataCache(newCheckedPrefCode, loadedPrefDataCache);
   }
+
+  newStates.newCheckedPrefCodes = updateCheckedPrefCodes(checked, checkedPrefCodes, newCheckedPrefCode);
+
+  return newStates;
 };
+
+const updateLoadedDataCache = async (newCheckedPrefCode: number, loadedPrefDataCache: Map<number, number[]>) => {
+  const res = await FetchPopulation(newCheckedPrefCode);
+  const newLoadedData = new Map(loadedPrefDataCache);
+  newLoadedData.set(
+    newCheckedPrefCode,
+    res[0].data.map((item: any) => item.value),
+  );
+  return newLoadedData;
+}
+
+const updateCheckedPrefCodes = (checked: boolean, checkedPrefCodes:number[], newCheckedPrefCode: number) => {
+  if (checked) {
+    if (!checkedPrefCodes.includes(newCheckedPrefCode)) {
+      return [...checkedPrefCodes, newCheckedPrefCode];
+    }
+  } else {
+    return checkedPrefCodes.filter(
+      (code) => code !== newCheckedPrefCode,
+    );
+  }
+}
