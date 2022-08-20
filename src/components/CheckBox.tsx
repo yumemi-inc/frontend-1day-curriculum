eff8038c103f9e27f426b9a1bf9775c8333fff38
ajfAfg <56056962+ajfAfg@users.noreply.github.com>
2022-08-20 16:38:00 +0900

    メモ化した

--- src/components/CheckBox.tsx
+++ src/components/CheckBox.tsx
@@ -1,11 +1,15 @@
+import React from "react"
+
 export type CheckBoxProps = {
   label: string
   onChange: React.ChangeEventHandler<HTMLInputElement>
 }
 
-export const CheckBox: React.FC<CheckBoxProps> = ({ label, onChange }) => {
+export const CheckBox: React.FC<CheckBoxProps> = React.memo(({ label, onChange }) => {
   return <label className="app-prefectures-list">
     <input className='app-prefectures-list-checkbox' type="checkbox" onChange={onChange}/>
     <span className='app-prefectures-name'>{ label }</span>
   </label>
-}
+})
+
+CheckBox.displayName = "CheckBox"
