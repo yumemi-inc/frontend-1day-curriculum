e4410c650f258cc20a7e4b45383a768ff68d1f4b
cathiecode <cathiecode@gmail.com>
2022-08-20 16:27:34 +0900

    refactor: GraphOptionをコンポーネント内部から独立させた

--- src/App.tsx
+++ src/App.tsx
@@ -22,13 +22,87 @@ export type SelectedPrefData = {
   }[];
 };
 
+type GraphData = { data: number[]; name: string }[];
+
+const getGraphOption = (graphData: GraphData) => ({
+  chart: {
+    type: "spline",
+    backgroundColor: "#fff",
+    polar: true,
+  },
+  title: {
+    text: "人口構成",
+  },
+  lang: {
+    noData: "データがありません",
+  },
+  noData: {
+    style: {
+      fontWeight: "bold",
+      fontSize: "15px",
+      color: "#303030",
+    },
+  },
+  xAxis: {
+    /* APIが返してくる年度はこれだった */
+    categories: [
+      "1960",
+      "1965",
+      "1970",
+      "1975",
+      "1980",
+      "1985",
+      "1990",
+      "1995",
+      "2000",
+      "2005",
+      "2010",
+      "2015",
+      "2020",
+      "2025",
+      "2030",
+      "2035",
+      "2040",
+      "2045",
+    ],
+    offset: 0,
+    title: {
+      text: "年度",
+      align: "high",
+      textAlign: "left",
+      rotation: 0,
+      offset: 0,
+      margin: 0,
+      y: 30,
+      x: -27,
+    },
+  },
+  yAxis: {
+    visible: true,
+    tickPosition: "inside",
+    offset: 0,
+    title: {
+      text: "人口数",
+      align: "high",
+      textAlign: "left",
+      rotation: 0,
+      offset: 0,
+      margin: 0,
+      y: -20,
+      x: -47,
+    },
+  },
+  series: graphData,
+})
+
+
 const App: React.FC = () => {
   const [prefAry, setPrefAry] = useState<PrefData[]>([])
   const [selectedPrefData, setSelectedPrefData] = useState<SelectedPrefData[]>(
     [],
   )
 
-  const graphData: { data: number[]; name: string }[] = selectedPrefData
+  const graphData: GraphData = selectedPrefData
     .filter(
       (pref) =>
         pref !== undefined &&
@@ -44,76 +118,7 @@ const App: React.FC = () => {
       }
     })
 
-  const options = {
-    chart: {
-      type: "spline",
-      backgroundColor: "#fff",
-      polar: true,
-    },
-    title: {
-      text: "人口構成",
-    },
-    lang: {
-      noData: "データがありません",
-    },
-    noData: {
-      style: {
-        fontWeight: "bold",
-        fontSize: "15px",
-        color: "#303030",
-      },
-    },
-    xAxis: {
-      /* APIが返してくる年度はこれだった */
-      categories: [
-        "1960",
-        "1965",
-        "1970",
-        "1975",
-        "1980",
-        "1985",
-        "1990",
-        "1995",
-        "2000",
-        "2005",
-        "2010",
-        "2015",
-        "2020",
-        "2025",
-        "2030",
-        "2035",
-        "2040",
-        "2045",
-      ],
-      offset: 0,
-      title: {
-        text: "年度",
-        align: "high",
-        textAlign: "left",
-        rotation: 0,
-        offset: 0,
-        margin: 0,
-        y: 30,
-        x: -27,
-      },
-    },
-    yAxis: {
-      visible: true,
-      tickPosition: "inside",
-      offset: 0,
-      title: {
-        text: "人口数",
-        align: "high",
-        textAlign: "left",
-        rotation: 0,
-        offset: 0,
-        margin: 0,
-        y: -20,
-        x: -47,
-      },
-    },
-    series: graphData,
-  }
+  const options = getGraphOption(graphData)
 
   useEffect(() => {
     getPrefData.GetPref().then((data) => setPrefAry(data))
