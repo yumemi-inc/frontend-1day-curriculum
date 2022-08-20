c80db311f4976b0330f3d81ba334b4d740d5df14
rmaejima <ryo.maejii@gmail.com>
2022-08-20 15:47:25 +0900

    refactor: make use PopulationGraph

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
 import "./App.css"
 import { fetchPopulation, fetchPrefecture } from "./api/resas"
+import { PopulationGraph } from "./components/PopulationGraph"
 import { makeNewStates } from "./makeNewStates"
-import Highcharts from "highcharts"
-import HighchartsReact from "highcharts-react-official"
-import NoDataToDisplay from "highcharts/modules/no-data-to-display"
 import { useState, useEffect } from "react"
-
-NoDataToDisplay(Highcharts)
 
 type PrefData = {
   prefCode: number
@@ -30,82 +26,10 @@ const App: React.FC = () => {
       data: [...loadedPrefData.get(pref!.prefCode)!],
     }))
 
-  const options = {
-    chart: {
-      type: "spline",
-      backgroundColor: "#fff",
-      polar: true,
-    },
-    title: {
-      text: "人口構成",
-    },
-    lang: {
-      noData: "データがありません",
-    },
-    noData: {
-      style: {
-        fontWeight: "bold",
-        fontSize: "15px",
-        color: "#303030",
-      },
-    },
-    xAxis: {
-      /* APIが返してくる年度はこれだった */
-      /*
-      categories: [
-        "1960",
-        "1965",
-        "1970",
-        "1975",
-        "1980",
-        "1985",
-        "1990",
-        "1995",
-        "2000",
-        "2005",
-        "2010",
-        "2015",
-        "2020",
-        "2025",
-        "2030",
-        "2035",
-        "2040",
-        "2045",
-      ],
-      */
-      categories: years,
-      offset: 0,
-      title: {
-        text: "年度",
-        align: "high",
-        textAlign: "left",
-        rotation: 0,
-        offset: 0,
-        margin: 0,
-        y: 30,
-        x: -27,
-      },
-    },
-    yAxis: {
-      visible: true,
-      tickPosition: "inside",
-      offset: 0,
-      title: {
-        text: "人口数",
-        align: "high",
-        textAlign: "left",
-        rotation: 0,
-        offset: 0,
-        margin: 0,
-        y: -20,
-        x: -47,
-      },
-    },
-    series: graphData,
-  }
-
   useEffect(() => {
-    fetchPopulation(1).then((popres) => setYears(popres[0].data.map((prefs) => prefs.year)))
+    fetchPopulation(1).then((popres) =>
+      setYears(popres[0].data.map((prefs) => prefs.year)),
+    )
     fetchPrefecture().then((data) => setPrefAry(data))
   }, [])
 
@@ -141,13 +65,7 @@ const App: React.FC = () => {
           )
         })}
       </div>
-      <div className="container-chart">
-        <HighchartsReact
-          highcharts={Highcharts}
-          constructorType={"chart"}
-          options={options}
-        />
-      </div>
+      <PopulationGraph years={years} graphData={graphData} />
     </div>
   )
 }
