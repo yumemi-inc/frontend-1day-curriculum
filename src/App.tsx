59d58cadbc7ff5bb5186c1d2a09e14e8c8ad502a
MasanobuRyuman <22011376mr@tama.ac.jp>
2022-08-20 12:47:01 +0900

    :construction: checkBoxのリファクタリング途中

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Highcharts from "highcharts"
 import HighchartsReact from "highcharts-react-official"
 import NoDataToDisplay from "highcharts/modules/no-data-to-display"
 import { makeNewStates } from "./makeNewStates"
+import { ChkBx01 } from "./components/ChkBx01"
 
 NoDataToDisplay(Highcharts)
 
@@ -124,14 +125,7 @@ const App: React.FC = () => {
       <div className='app-prefectures-list-container'>
         {prefAry?.map((item) => {
           return (
-            <label key={item.prefCode} className='app-prefectures-list'>
-              <input
-                className='app-prefectures-list-checkbox'
-                type='checkbox'
-                onChange={(e) => handleChange(e.target.checked, item.prefCode)}
-              />
-              <span className='app-prefectures-name'>{item.prefName}</span>
-            </label>
+            <ChkBx01 key={item.prefCode} name={item.prefName}  onChange={(e) => handleChange(e, item.prefCode)} checked={true}/>
           )
         })}
       </div>
--- src/components/ChkBx01.tsx
+++ src/components/ChkBx01.tsx
@@ -1,14 +1,18 @@
-import { useState } from "react"
+import { useCallback } from "react"
 
 export type ChkBx01Props = {
   name: string
+  checked: boolean
+  onChange: (checked:boolean)=> void
 }
 
 // 都道府県1つ分のチェックボックス
-export const ChkBx01: React.FC<ChkBx01Props> = () => {
-  const [checked, setChecked] = useState(false)
-  return <label>
-    <input type="checkbox" checked={checked} onChange={() => setChecked(bool => !bool)}/>
-    <span>愛知県</span>
+export const ChkBx01: React.FC<ChkBx01Props> = (props:ChkBx01Props) => {
+  const handleChange = useCallback((event:any)=>{
+    props.onChange(event.currentTarget.checked)  
+  },[props.onChange])
+  return <label className='app-prefectures-list'>
+    <input type="checkbox" checked={props.checked} onChange={handleChange} className='app-prefectures-list-checkbox'/>
+    <span className='app-prefectures-name'>{props.name}</span>
   </label>
 }
