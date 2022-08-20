9347811f49f9277796c6e3c1570334b6c435930c
Hoshi Kenya <hoshimure.47@gmail.com>
2022-08-20 14:21:47 +0900

    fix: fetchの処理を分けた

--- src/makeNewStates.ts
+++ src/makeNewStates.ts
@@ -1,35 +1,46 @@
 import getPopData from "./components/popAPI"
 
+const fetchNewData = async (  
+  checked: boolean,
+  prefCode: number,
+  loadedPrefData: Map<number, number[]>,
+) => {
+  if (!checked || loadedPrefData.has(prefCode)) return loadedPrefData
+
+  const res = await getPopData.FetchPop(prefCode)
+  const newLoadedData = new Map(loadedPrefData)
+  newLoadedData.set(
+    prefCode,
+    res[0].data.map((item: any) => item.value),
+  )
+
+  return newLoadedData
+}
+
 export const makeNewStates = async (
   checked: boolean,
   prefCode: number,
   checkedPrefCodes: number[],
   loadedPrefData: Map<number, number[]>,
 ) => {
+  const fetchedNewLoadData = await fetchNewData(checked, prefCode, loadedPrefData)
+
   const newStates = {
     newCheckedPrefCodes: checkedPrefCodes,
-    fetchedNewLoadData: loadedPrefData,
+    fetchedNewLoadData,
   }
+
+  // チェックが入った場合
   if (checked) {
     if (!checkedPrefCodes.includes(prefCode)) {
       newStates.newCheckedPrefCodes = [...checkedPrefCodes, prefCode]
     }
-    if (!loadedPrefData.has(prefCode)) {
-      const res = await getPopData.FetchPop(prefCode)
 
-      const newLoadedData = new Map(loadedPrefData)
-      newLoadedData.set(
-        prefCode,
-        res[0].data.map((item: any) => item.value),
-      )
-
-      newStates.fetchedNewLoadData = newLoadedData
-    }
-    return newStates
+  // チェックが外れた場合
   } else {
     newStates.newCheckedPrefCodes = checkedPrefCodes.filter(
       (code) => code !== prefCode,
     )
-    return newStates
   }
+  return newStates
 }
